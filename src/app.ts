7950f471d11eae07f02f1db1e46446d9c5241db9
TanbirHasan <mdtanbir576@gmail.com>
2024-06-29 11:10:01 +0600

    express added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 
-import express from "express"
-const app = express()
+import express, { Application,Request,Response } from "express"
+import cors from "cors"
+const app : Application = express()
 const port = 3000
 
- app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+
+ app.get('/', (req:Request, res:Response) => {
+  res.send('Server is connected successfully')
 })
 
 
