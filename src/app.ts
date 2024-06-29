fabdeb6f6facb73c3c1fe726d4a66631431f251b
TanbirHasan <mdtanbir576@gmail.com>
2024-06-29 12:13:02 +0600

    fixed

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,7 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-
-
-    app.get('/', (req: Requests, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Server is connected suc')
 })
 
