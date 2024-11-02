ea92b17ff725a23245d1f50437e6273e4c1730d9
TanbirHasan <mdtanbir576@gmail.com>
2024-11-02 23:16:50 +0600

    updated user service

--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -4,15 +4,17 @@ import { IUser } from './user.interface'
 import { generateUserId } from './users.utils'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
-
+  // auto generated incremental id
   const id = await generateUserId()
-  user.id = id;
+  user.id = id
 
-
-  if(!user.password){
-    user.password = config.default_user_pass as string;
+  if (!user.password) {
+    user.password = config.default_user_pass as string
   }
+
+  console.log(user)
   const createUser = await User.create(user)
+  console.log(createUser)
 
   if (!createUser) {
     throw new Error('Failed to create user!')
