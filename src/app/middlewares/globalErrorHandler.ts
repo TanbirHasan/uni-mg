c6793c95b0cc03d0751a1b247b9e4a86b49a4fe1
TanbirHasan <mdtanbir576@gmail.com>
2024-11-30 11:38:46 +0600

    zod error validation added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -4,6 +4,8 @@ import { IGenericErrorMessage } from '../../interfaces/error'
 import handleValidationError from '../../errors/handleValidationError'
 import ApiError from '../../errors/ApiError'
 import { errorLogger } from '../../shared/logger'
+import { ZodError } from 'zod'
+import handleZodError from '../../errors/handleZodError'
 
 const globalErrorHandler = (
   error: any,
@@ -21,6 +23,11 @@ const globalErrorHandler = (
 
   if (error?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error)
     statusCode = simplifiedError.statusCode
     message = simplifiedError.message
     errorMessages = simplifiedError.errorMessages
--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,40 @@ import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
 import { logger, errorLogger } from './shared/logger'
+import { Server } from 'http'
 
 const port = 4000
 
-async function server() {
+process.on('uncaughtException', err => {
+  errorLogger.error(err)
+  process.exit(1)
+})
+
+let server: Server
+
+async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
     logger.info('Database connected successfully')
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`application listening on port ${config.port}`)
     })
   } catch (err) {
     errorLogger.error('failed to connect server')
   }
+
+  process.on('unhandledRejection', err => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(err)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 
-server()
+bootstrap()
+
+// SIGTERM for detecting suddden crash
