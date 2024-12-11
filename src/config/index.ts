623ac1274e6e6a8c123d217496a25ec8b6e9885a
TanbirHasan <mdtanbir576@gmail.com>
2024-12-11 23:22:27 +0600

    acadmeicDepartment added

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,4 +8,8 @@ export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   default_user_pass: process.env.DEFAULT_USER_PASSWORD,
+  redis: {
+    url: process.env.REDIS_URL,
+    expires_in: process.env.REDIS_TOKEN_EXPIRES_IN,
+  },
 }
